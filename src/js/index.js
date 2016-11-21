eb9b47a229f56a59e0022ecadfaa38a50df6261f
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-11-21 16:17:20 -0500

    created data and reducers for my store

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 // Javascript Entry Point
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import allReducers from './reducers';
+
+
+const store = createStore(allReducers);
---
+++ src/js/reducers/reduce-users.js
@@ -0,0 +1,25 @@
+
+
+
+export default function() {
+	return [
+		{
+			id: 1,
+			name: 'Joshua',
+			age: 36,
+			thumbnail: '../images/images.jpeg'
+		},
+		{
+			id: 2,
+			name: 'Jeremiah',
+			age: 39,
+			thumbnail: '../images/jeremiah.jpg'
+		},
+		{
+			id: 3,
+			name: 'Randall',
+			age: 50,
+			thumbnail: '../images/randall.jpg'
+		}
+			]
+};
